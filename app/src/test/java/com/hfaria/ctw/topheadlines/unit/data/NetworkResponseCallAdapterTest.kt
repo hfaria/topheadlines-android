7ccac8470bd9b6e15147c332e37d6bb5be55cc3d
Henrique Faria <hfaria020@gmail.com>
2023-02-04 20:44:45 -0300

    Created NetworkResponseCallAdapterTest success scenarios

--- app/src/test/java/com/hfaria/ctw/topheadlines/unit/data/NetworkResponseCallAdapterTest.kt
+++ app/src/test/java/com/hfaria/ctw/topheadlines/unit/data/NetworkResponseCallAdapterTest.kt
@@ -1,10 +1,13 @@
 package com.hfaria.ctw.topheadlines.unit.data
 
 import com.hfaria.ctw.topheadlines.data.network.*
+import com.hfaria.ctw.topheadlines.unit.data.RetrofitFakeResponses.EMPTY_RESPONSE
 import com.hfaria.ctw.topheadlines.unit.data.RetrofitFakeResponses.EXCEPTION
+import com.hfaria.ctw.topheadlines.unit.data.RetrofitFakeResponses.SUCCESS_RESPONSE
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.runBlocking
 import org.junit.Assert.assertEquals
+import org.junit.Assert.assertTrue
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -12,8 +15,17 @@ import org.mockito.Mock
 import org.mockito.Mockito.`when`
 import org.mockito.junit.MockitoJUnitRunner
 import retrofit2.Call
+import retrofit2.Response
 
 object RetrofitFakeResponses {
+
+    val SUCCESS_RESPONSE = Response.success(
+        GetTopHeadlinesResponse()
+    )
+
+    val EMPTY_RESPONSE = Response.success<GetTopHeadlinesResponse>(
+        null
+    )
 
     val EXCEPTION = Exception("RetrofitFakeException")
 }
@@ -30,6 +42,25 @@ class NetworkResponseCallAdapterTest {
     @Before
     fun setup() {
         adapter = NetworkResponseCallAdapter(GetTopHeadlinesResponse::class.java)
+    }
+
+    @Test
+    fun `Should handle success response`() = runBlocking {
+        `when`(call.execute()).thenReturn(SUCCESS_RESPONSE)
+
+        val expected = SuccessNetworkResponse(
+            GetTopHeadlinesResponse()
+        )
+
+        val actual = adapter.adapt(call)
+        assertEquals(expected.toString(), actual.toString())
+    }
+
+    @Test
+    fun `Should handle empty response`() = runBlocking {
+        `when`(call.execute()).thenReturn(EMPTY_RESPONSE)
+        val networkResponse = adapter.adapt(call)
+        assertTrue(networkResponse is EmptyNetworkResponse)
     }
 
     @Test
