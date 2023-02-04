d8158a6deda66d9290741443cecb465d562af389
Henrique Faria <hfaria020@gmail.com>
2023-02-04 16:39:50 -0300

    InMemoryTopHeadlinesPagingSourceTest: Added page loading tests for success scenarios

--- app/src/test/java/com/hfaria/ctw/topheadlines/unit/data/InMemoryTopHeadlinesPagingSourceTest.kt
+++ app/src/test/java/com/hfaria/ctw/topheadlines/unit/data/InMemoryTopHeadlinesPagingSourceTest.kt
@@ -2,10 +2,15 @@ package com.hfaria.ctw.topheadlines.unit.data
 
 import androidx.paging.PagingSource
 import com.hfaria.ctw.topheadlines.data.network.GetTopHeadlinesResponse
+import com.hfaria.ctw.topheadlines.data.network.NetworkResponse
 import com.hfaria.ctw.topheadlines.data.network.SuccessNetworkResponse
 import com.hfaria.ctw.topheadlines.data.network.TopHeadlinesApi
 import com.hfaria.ctw.topheadlines.data.repository.InMemoryTopHeadlinesPagingSource
 import com.hfaria.ctw.topheadlines.domain.Article
+import com.hfaria.ctw.topheadlines.unit.data.GetTopHeadlinesFakeResponses.ARTICLES
+import com.hfaria.ctw.topheadlines.unit.data.GetTopHeadlinesFakeResponses.ARTICLES_LAST_PAGE
+import com.hfaria.ctw.topheadlines.unit.data.GetTopHeadlinesFakeResponses.SUCCESS_RESPONSE
+import com.hfaria.ctw.topheadlines.unit.data.GetTopHeadlinesFakeResponses.SUCCESS_RESPONSE_LAST_PAGE
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.withContext
@@ -18,14 +23,31 @@ import org.mockito.ArgumentMatchers.anyString
 import org.mockito.Mock
 import org.mockito.Mockito.`when`
 import org.mockito.junit.MockitoJUnitRunner
+import kotlin.coroutines.CoroutineContext
 
-val ARTICLES = listOf(Article("A1"), Article("A2"))
-val SUCCESS_RESPONSE = SuccessNetworkResponse(
-    GetTopHeadlinesResponse(
-        totalResults = ARTICLES.size,
-        articles = ARTICLES
+object GetTopHeadlinesFakeResponses {
+    val ARTICLES = listOf(
+        Article("A1"),
+        Article("A2"),
+        Article("A3"),
+        Article("A4"),
+        Article("A5"),
     )
-)
+    val ARTICLES_LAST_PAGE = ARTICLES.subList(0, ARTICLES.lastIndex - 2)
+
+    val SUCCESS_RESPONSE = SuccessNetworkResponse(
+        GetTopHeadlinesResponse(
+            totalResults = ARTICLES.size,
+            articles = ARTICLES
+        )
+    )
+    val SUCCESS_RESPONSE_LAST_PAGE = SuccessNetworkResponse(
+        GetTopHeadlinesResponse(
+            totalResults = ARTICLES_LAST_PAGE.size,
+            articles = ARTICLES_LAST_PAGE
+        )
+    )
+}
 
 @RunWith(MockitoJUnitRunner::class)
 @OptIn(ExperimentalCoroutinesApi::class)
@@ -38,31 +60,74 @@ class InMemoryTopHeadlinesPagingSourceTest {
 
     @Before
     fun setup() {
-        source = InMemoryTopHeadlinesPagingSource(api, 2)
+        source = InMemoryTopHeadlinesPagingSource(api, ARTICLES.size)
     }
 
-    @Test
-    fun `Load first page`() = runBlocking {
+    private suspend fun runPageLoadingTest(
+        context: CoroutineContext,
+        curPage: Int?,
+        apiResponse: NetworkResponse<GetTopHeadlinesResponse>,
+        expected: PagingSource.LoadResult.Page<Int, Article>
+    ) {
         `when`(api.getTopHeadlines(
             anyString(),
             anyInt(),
             anyInt()
-        )).thenReturn(SUCCESS_RESPONSE)
+        )).thenReturn(apiResponse)
 
-        val expected = PagingSource.LoadResult.Page(
-            data = ARTICLES,
-            prevKey = null,
-            nextKey = 2
-        )
-        val actual = withContext(this.coroutineContext) {
+        val actual = withContext(context) {
             source.load(
                 PagingSource.LoadParams.Refresh(
-                    key = null,
-                    loadSize = 2,
+                    key = curPage,
+                    loadSize = ARTICLES.size,
                     placeholdersEnabled = false
                 )
             )
         }
+
         assertEquals(expected.toString(), actual.toString())
+    }
+
+    @Test
+    fun `Load first page`() = runBlocking {
+        // First Page
+        runPageLoadingTest(
+            context = this.coroutineContext,
+            curPage = null,
+            apiResponse = SUCCESS_RESPONSE,
+            expected = PagingSource.LoadResult.Page(
+                data = ARTICLES,
+                prevKey = null,
+                nextKey = 2
+            )
+        )
+    }
+
+    @Test
+    fun `Load middle page`() = runBlocking {
+        runPageLoadingTest(
+            context = this.coroutineContext,
+            curPage = 4,
+            apiResponse = SUCCESS_RESPONSE,
+            expected = PagingSource.LoadResult.Page(
+                data = ARTICLES,
+                prevKey = null,
+                nextKey = 5
+            )
+        )
+    }
+
+    @Test
+    fun `Load last page`() = runBlocking {
+        runPageLoadingTest(
+            context = this.coroutineContext,
+            curPage = 4,
+            apiResponse = SUCCESS_RESPONSE_LAST_PAGE,
+            expected = PagingSource.LoadResult.Page(
+                data = ARTICLES_LAST_PAGE,
+                prevKey = null,
+                nextKey = null
+            )
+        )
     }
 }
