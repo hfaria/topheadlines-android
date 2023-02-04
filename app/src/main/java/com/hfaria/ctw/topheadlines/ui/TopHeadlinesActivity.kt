16f20af0bc00b4d59b5456f9bf374fc3b6c78c88
Henrique Faria <hfaria020@gmail.com>
2023-02-03 21:45:34 -0300

    Changed screen title to match selected news provider and implemented back navigation in ArticleContentActivity

--- app/src/main/java/com/hfaria/ctw/topheadlines/ui/TopHeadlinesActivity.kt
+++ app/src/main/java/com/hfaria/ctw/topheadlines/ui/TopHeadlinesActivity.kt
@@ -1,17 +1,16 @@
 package com.hfaria.ctw.topheadlines.ui
 
-import android.content.Context
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.commit
 import androidx.fragment.app.add
+import com.hfaria.ctw.topheadlines.BuildConfig
 import com.hfaria.ctw.topheadlines.R
-import com.hfaria.ctw.topheadlines.domain.Article
 
 class TopHeadlinesActivity : AppCompatActivity(R.layout.activity_top_headlines) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setTitle(BuildConfig.NEWS_SOURCE_TITLE)
         if (savedInstanceState == null) {
             supportFragmentManager.commit {
                 add<TopHeadlinesFragment>(R.id.fragment_container_view)
--- app/src/main/java/com/hfaria/ctw/topheadlines/ui/article_content/ArticleContentActivity.kt
+++ app/src/main/java/com/hfaria/ctw/topheadlines/ui/article_content/ArticleContentActivity.kt
@@ -2,21 +2,29 @@ package com.hfaria.ctw.topheadlines.ui.article_content
 
 import android.content.Context
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.fragment.app.commit
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.add
+import androidx.fragment.app.commit
+import com.hfaria.ctw.topheadlines.BuildConfig
 import com.hfaria.ctw.topheadlines.R
 import com.hfaria.ctw.topheadlines.domain.Article
 
 class ArticleContentActivity : AppCompatActivity(R.layout.activity_article_content) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setTitle(BuildConfig.NEWS_SOURCE_TITLE)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
         if (savedInstanceState == null) {
             supportFragmentManager.commit {
                 add<ArticleContentFragment>(R.id.fragment_container_view)
             }
         }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        finish()
+        return true
     }
 
     companion object {
