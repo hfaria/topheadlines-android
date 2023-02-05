b0a3fec5ffcf68c38dd5050f0b7a21ce26efa3ca
Henrique Faria <hfaria020@gmail.com>
2023-02-05 16:28:17 -0300

    Created InMemoryTopHeadlinesPagingSourceGetRefreshKeyTest class

--- app/src/test/java/com/hfaria/ctw/topheadlines/unit/data/InMemoryTopHeadlinesPagingSourceTest.kt
+++ app/src/test/java/com/hfaria/ctw/topheadlines/unit/data/InMemoryTopHeadlinesPagingSourceTest.kt
@@ -1,6 +1,8 @@
 package com.hfaria.ctw.topheadlines.unit.data
 
+import androidx.paging.PagingConfig
 import androidx.paging.PagingSource
+import androidx.paging.PagingState
 import com.hfaria.ctw.topheadlines.data.network.*
 import com.hfaria.ctw.topheadlines.data.repository.InMemoryTopHeadlinesPagingSource
 import com.hfaria.ctw.topheadlines.data.repository.InMemoryTopHeadlinesPagingSource.Companion.API_ERROR
@@ -26,6 +28,7 @@ import org.mockito.ArgumentMatchers.anyString
 import org.mockito.Mock
 import org.mockito.Mockito.`when`
 import org.mockito.junit.MockitoJUnitRunner
+import retrofit2.Call
 import kotlin.coroutines.CoroutineContext
 
 object GetTopHeadlinesFakeResponses {
@@ -71,7 +74,7 @@ object GetTopHeadlinesFakeResponses {
 
 @RunWith(MockitoJUnitRunner::class)
 @OptIn(ExperimentalCoroutinesApi::class)
-class InMemoryTopHeadlinesPagingSourceTest {
+class InMemoryTopHeadlinesPagingSourceLoadTest {
 
     @Mock
     lateinit var api: TopHeadlinesApi
@@ -245,5 +248,74 @@ class InMemoryTopHeadlinesPagingSourceTest {
         }
 
         assertEquals(expected.toString(), actual.toString())
+    }
+}
+
+@RunWith(MockitoJUnitRunner::class)
+@OptIn(ExperimentalCoroutinesApi::class)
+class InMemoryTopHeadlinesPagingSourceGetRefreshKeyTest {
+
+    @Mock
+    lateinit var api: TopHeadlinesApi
+
+    lateinit var source: InMemoryTopHeadlinesPagingSource
+
+    @Before
+    fun setup() {
+        source = InMemoryTopHeadlinesPagingSource(api, ARTICLES.size)
+    }
+
+    @Test
+    fun `getRefreshKey should return the next key when prevKey is null`() {
+        // Given
+        val state = createPagingState(prevKey = null, nextKey = 3)
+
+        // When
+        val refreshKey = source.getRefreshKey(state)
+
+        // Then
+        assertEquals(2, refreshKey)
+    }
+
+    @Test
+    fun `getRefreshKey should return the prev key when nextKey is null`() {
+        // Given
+        val state = createPagingState(prevKey = 1, nextKey = null)
+
+        // When
+        val refreshKey = source.getRefreshKey(state)
+
+        // Then
+        assertEquals(2, refreshKey)
+    }
+
+    @Test
+    fun `getRefreshKey should return null when both prevKey and nextKey are null`() {
+        // Given
+        val state = createPagingState(prevKey = null, nextKey = null)
+
+        // When
+        val refreshKey = source.getRefreshKey(state)
+
+        // Then
+        assertEquals(null, refreshKey)
+    }
+
+    private fun createPagingState(prevKey: Int?, nextKey: Int?): PagingState<Int, Article> {
+        val closestPage = createClosestPage(prevKey, nextKey)
+        return PagingState(
+            pages = listOf(closestPage),
+            anchorPosition = 0,
+            config = PagingConfig(pageSize = 1),
+            leadingPlaceholderCount = 0
+        )
+    }
+
+    private fun createClosestPage(prevKey: Int?, nextKey: Int?): PagingSource.LoadResult.Page<Int, Article> {
+        return PagingSource.LoadResult.Page(
+            data = listOf(Article(title = "FOOBAR")),
+            prevKey = prevKey,
+            nextKey = nextKey
+        )
     }
 }
