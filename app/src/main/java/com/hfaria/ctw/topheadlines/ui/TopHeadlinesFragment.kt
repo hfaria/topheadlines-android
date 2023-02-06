ac6cb443d93fb4f34e423e688cb50daf9f8a7aeb
Henrique Faria <hfaria020@gmail.com>
2023-02-06 13:45:18 -0300

    Added Biometric Authentication to TopHeadlinesFragment

--- app/src/main/java/com/hfaria/ctw/topheadlines/ui/TopHeadlinesFragment.kt
+++ app/src/main/java/com/hfaria/ctw/topheadlines/ui/TopHeadlinesFragment.kt
@@ -1,11 +1,20 @@
 package com.hfaria.ctw.topheadlines.ui
 
+import android.content.Context
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.biometric.BiometricManager
+import androidx.biometric.BiometricManager.Authenticators.BIOMETRIC_WEAK
+import androidx.biometric.BiometricPrompt
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.lifecycleScope
+import com.hfaria.ctw.topheadlines.BuildConfig
+import com.hfaria.ctw.topheadlines.R
 import com.hfaria.ctw.topheadlines.databinding.FragmentTopHeadlinesBinding
 import com.hfaria.ctw.topheadlines.ui.article_content.ArticleContentActivity
 import com.hfaria.ctw.topheadlines.ui.base.BaseFragment
@@ -15,6 +24,7 @@ class TopHeadlinesFragment : BaseFragment<TopHeadlinesViewModel>() {
 
     private lateinit var binding: FragmentTopHeadlinesBinding
     private lateinit var adapter: TopHeadlinesAdapter
+    private val allowedAuthenticators = BIOMETRIC_WEAK
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -34,6 +44,12 @@ class TopHeadlinesFragment : BaseFragment<TopHeadlinesViewModel>() {
         setupObservables(viewModel.state)
         setupRoutes(viewModel.state)
         viewModel.getTopHeadlines()
+
+        if (BuildConfig.ENABLE_FINGERPRINT_SCANNING &&
+            Build.VERSION.SDK_INT >= Build.VERSION_CODES.M &&
+            isBiometricAuthAvailable()) {
+            showBiometricPrompt()
+        }
     }
 
     private fun setupObservables(state: TopHeadlinesScreenState) {
@@ -54,6 +70,51 @@ class TopHeadlinesFragment : BaseFragment<TopHeadlinesViewModel>() {
         state.articleProfileRoute.observe(viewLifecycleOwner) { article->
             ArticleContentActivity.open(requireContext(), article)
         }
+    }
+
+    private fun isBiometricAuthAvailable(): Boolean {
+        val manager = BiometricManager.from(requireContext())
+        return manager.canAuthenticate(allowedAuthenticators) == BiometricManager.BIOMETRIC_SUCCESS
+    }
+
+    private fun showBiometricPrompt() {
+        val appContext = requireActivity().applicationContext
+        val executor = ContextCompat.getMainExecutor(appContext)
+        val biometricPrompt = BiometricPrompt(this, executor,
+            object : BiometricPrompt.AuthenticationCallback() {
+                override fun onAuthenticationError(errorCode: Int,
+                                                   errString: CharSequence) {
+                    super.onAuthenticationError(errorCode, errString)
+                    val msg = getString(R.string.biometric_login_auth_error_message, errString, errorCode.toString())
+                    showErrorDialog(appContext, msg)
+                }
+
+                override fun onAuthenticationFailed() {
+                    super.onAuthenticationFailed()
+                    val msg = getString(R.string.biometric_login_auth_failed_message)
+                    showErrorDialog(appContext, msg)
+                }
+            })
+
+        val promptInfo = BiometricPrompt.PromptInfo.Builder()
+            .setTitle(getString(R.string.biometric_login_dialog_title))
+            .setSubtitle(getString(R.string.biometric_login_dialog_subtitle))
+            .setAllowedAuthenticators(allowedAuthenticators)
+            .build()
+
+        biometricPrompt.authenticate(promptInfo)
+    }
+
+    private fun showErrorDialog(ctx: Context, message: String) {
+        AlertDialog.Builder(ctx)
+            .setTitle(R.string.error_dialog_title)
+            .setMessage(message)
+            .setPositiveButton(R.string.error_dialog_ok_button) { _, _ ->
+                requireActivity().finish()
+            }
+            .setCancelable(false)
+            .create()
+            .show()
     }
 }
 
